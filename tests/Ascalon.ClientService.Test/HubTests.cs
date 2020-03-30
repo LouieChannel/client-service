fa3bc5e168a9991fd09a154d877e1f231019b467
a.mirko <a.mirko@dostaevsky.ru>
2020-03-30 19:24:26 +0300

    #master - добавил в проект дополнительную проверку на consumer

--- tests/Ascalon.ClientService.Test/HubTests.cs
+++ tests/Ascalon.ClientService.Test/HubTests.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SignalR.Client;
 using System;
 using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
@@ -18,6 +19,16 @@ namespace Ascalon.ClientService.Test
         {
             try
             {
+                var driverConnection = new HubConnectionBuilder()
+                    .WithUrl("http://localhost:5000/logist", options =>
+                    {
+                        options.Cookies = new CookieContainer();
+                        options.Cookies.Add(new Cookie("Id", "2") { Domain = "localhost" });
+                        options.Cookies.Add(new Cookie("Role", "Driver") { Domain = "localhost" });
+                        options.Cookies.Add(new Cookie("Name", "Test1") { Domain = "localhost" });
+                    })
+                    .Build();
+
                 var logistConnection = new HubConnectionBuilder()
                     .WithUrl("http://localhost:5000/logist", options => 
                     {
@@ -28,18 +39,50 @@ namespace Ascalon.ClientService.Test
                     })
                     .Build();
 
+                var logist2Connection = new HubConnectionBuilder()
+                    .WithUrl("http://localhost:5000/logist", options =>
+                    {
+                        options.Cookies = new CookieContainer();
+                        options.Cookies.Add(new Cookie("Id", "3") { Domain = "localhost" });
+                        options.Cookies.Add(new Cookie("Role", "Logist") { Domain = "localhost" });
+                        options.Cookies.Add(new Cookie("Name", "Test") { Domain = "localhost" });
+                    })
+                    .Build();
+                
+                driverConnection.On<object>(nameof(LogistHub.CreateTask), orderItem =>
+                {
+                    Assert.NotNull(orderItem);
+                });
+
                 logistConnection.On<object>(nameof(LogistHub.CreateTask), orderItem =>
                 {
                     Assert.NotNull(orderItem);
                 });
 
+                logist2Connection.On<object>(nameof(LogistHub.CreateTask), orderItem =>
+                {
+                    Assert.NotNull(orderItem);
+                });
+
+                await driverConnection.StartAsync();
+
                 await logistConnection.StartAsync();
 
-                await logistConnection.InvokeAsync(nameof(LogistHub.CreateTask), "{\"DriverId\":1,\"Description\":\"test\",\"StartLongitude\":50.1354,\"StartLatitude\":30.4324,\"EndLongitude\":1.4342,\"EndLatitude\":43.1234,\"Status\":1,\"Entity\":\"dasfas\"}");
+                await logist2Connection.StartAsync();
 
-                await Task.Delay(3000);
+                await driverConnection.InvokeAsync(nameof(LogistHub.CreateTask), "{\"DriverId\":2,\"Description\":\"test21\",\"StartLongitude\":50.1354,\"StartLatitude\":30.4324,\"EndLongitude\":1.4342,\"EndLatitude\":43.1234,\"Status\":1,\"Entity\":\"dasfas\"}");
+
+                await logistConnection.InvokeAsync(nameof(LogistHub.CreateTask), "{\"DriverId\":2,\"Description\":\"test\",\"StartLongitude\":50.1354,\"StartLatitude\":30.4324,\"EndLongitude\":1.4342,\"EndLatitude\":43.1234,\"Status\":2,\"Entity\":\"dasfas\"}");
+
+                Thread.Sleep(10000);
+
+                await logist2Connection.InvokeAsync(nameof(LogistHub.CreateTask), "{\"DriverId\":2,\"Description\":\"test\",\"StartLongitude\":50.1354,\"StartLatitude\":30.4324,\"EndLongitude\":1.4342,\"EndLatitude\":43.1234,\"Status\":2,\"Entity\":\"dasfas\"}");
+
+                await driverConnection.StopAsync();
 
                 await logistConnection.StopAsync();
+
+                await logist2Connection.StopAsync();
             }
             catch(Exception ex)
             {
